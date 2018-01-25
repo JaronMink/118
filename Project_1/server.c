35d5e2ea86facf3993c17e50d30b50f1b8c44cea
jugeeya <jugeeya@live.com>
2018-01-25 14:46:28 -0800

    html files working

--- Project_1/server.c
+++ Project_1/server.c
@@ -28,10 +28,10 @@ void readFileContent(int fileFD, char** content, int* contentLen) {
   if(fstat(fileFD, &st) < 0) {
     error("ERROT: cannot read requested files stats");
   }
-  
-  int fileLen = st.st_size; //byte size of file                                                                                                                
+
+  int fileLen = st.st_size; //byte size of file
   char* fileStr = (char*) malloc(sizeof(char) * fileLen);
-  
+
   int bytesTotal = 0;
   int bytesRead;
   while((bytesRead = read(fileFD, (fileStr + bytesTotal), fileLen)) > 0) {
@@ -41,24 +41,30 @@ void readFileContent(int fileFD, char** content, int* contentLen) {
     error("ERROR: cannot read from specified file");
   }
 
-  //return contents and length                                                                                                                                  
+  //return contents and length
   *content = fileStr;
   *contentLen = fileLen;
   return;
 }
 
 
-void sendFileContent(int sockFD, int fileFD) {
+void sendFileContent(int sockFD, int fileFD, char* httpResponse) {
   char* fileContent = NULL;
   int fileLen = -1;
   readFileContent(fileFD, &fileContent, &fileLen);
-  write(sockFD, fileContent, fileLen); //might have to be put into while loop for large files                                             
-  //writeFileContent(fileContent, fileLen);                                                                                               
+  strcat(httpResponse, "Content-Length: %i\n");
+  strcat(httpResponse, "\n");
+  strcat(httpResponse, "");
+  char* fullResponse = (char*) malloc(sizeof(char) * (fileLen + strlen(httpResponse)));
+  sprintf(fullResponse, httpResponse, fileLen);
+  strcat(fullResponse, fileContent);
+  write(sockFD, fullResponse, strlen(fullResponse)); //might have to be put into while loop for large files
+  //writeFileContent(fileContent, fileLen);
 }
 
 void send404Error(int socketFD) {
   char* error404Msg = "Error 404, resource not found";
-  write(socketFD, error404Msg, strlen(error404Msg)); 
+  write(socketFD, error404Msg, strlen(error404Msg));
 }
 
 int main(int argc, char *argv[])
@@ -103,7 +109,7 @@ int main(int argc, char *argv[])
     n = read(newsockfd, buffer, 255);
     if (n < 0) error("ERROR reading from socket");
     printf("Here is the message: %s\n", buffer);
-	
+
     char file_path[256];
 
     // tokenize to get every spaced word
@@ -119,42 +125,41 @@ int main(int argc, char *argv[])
     {
       line = strtok_r(request_lines, line_tok, &saveptr_line);
       if (line == NULL)
-	break;
+	     break;
       //printf("%s\n\n", line);
       for (int j = 0; ; j++, line = NULL) {
-	word = strtok_r(line, word_tok, &saveptr_word);
-	if (word == NULL)
-	  break;
+      	word = strtok_r(line, word_tok, &saveptr_word);
+      	if (word == NULL)
+      	  break;
 
-	if (i == 0) // header line
-	{
-	  if (j == 0){
-	    if (strcmp("GET", word) == 0)
-	      {
-	    
-	      }
-	    else
-	      {
-	    
-	      }
-	  }
-	  if (j == 1){
-	    strcpy(file_path, word);
-	  }
-	  if (j == 2){
-	    if (strcmp ("HTTP/1.0", word) != 0)
-	      {	
-		//error("ERROR only supports HTTP 1.0.");
-		//close(newsockfd);
-		//close(sockfd);
-	      }
-	  }
-	}
-	//printf("%s ", word);
+      	if (i == 0) // header line
+      	{
+      	  if (j == 0){
+      	    if (strcmp("GET", word) == 0)
+      	      {
+
+      	      }
+      	    else
+      	      {
+
+      	      }
+      	  }
+      	  if (j == 1){
+      	    strcpy(file_path, word);
+      	  }
+      	  if (j == 2){
+      	    if (strcmp ("HTTP/1.0", word) != 0)
+      	      {
+            		//error("ERROR only supports HTTP 1.0.");
+            		//close(newsockfd);
+            		//close(sockfd);
+      	      }
+      	  }
+      	}
+      	//printf("%s ", word);
       }
-      printf("\n");
     }
-    
+
     char response[1024];
     char date_text[512];
     time_t now = time(0);
@@ -162,21 +167,23 @@ int main(int argc, char *argv[])
     strftime(date_text, sizeof(date_text), "%a, %d %b %Y %H:%M:%S %Z", &tm);
     struct stat attr;
     char last_modified[512];
-    stat(file_path, &attr);
-    strftime(last_modified, sizeof(last_modified), "%a, %d %b %Y %H:%M:%S%Z", gmtime(&(attr.st_ctime)));struct stat attr;
-    char last_modified[512];
-    char format_str = "HTTP/1.0 200 OK\nDate: %s\nServer: localhost\nMIME-version: 1.0\nLast-Modified: %s", date_text, last_modified);
-    sprintf(response, format_str, date_text);
+    stat(file_path+1, &attr);
+    strftime(last_modified, sizeof(last_modified), "%a, %d %b %Y %H:%M:%S %Z", gmtime(&(attr.st_mtime)));
+    char format_str[512] = "HTTP/1.0 200 OK\n";
+    strcat(format_str, "Date: %s\n");
+    strcat(format_str, "Server: localhost\n");
+    strcat(format_str, "MIME-version: 1.0\n");
+    strcat(format_str, "Last-Modified: %s\n");
+    strcat(format_str, "Content-Type: text/html\n");
+    sprintf(response, format_str, date_text, last_modified, attr.st_size);
 
-    printf("%s", response);
-    
     int requestedFD;
-    if((requestedFD = open("test.txt", O_RDONLY)) < 0) {
+    if((requestedFD = open(file_path+1, O_RDONLY)) < 0) {
       send404Error(newsockfd);
     } else {
-      sendFileContent(newsockfd, requestedFD);
+      sendFileContent(newsockfd, requestedFD, response);
     }
-    
+
     //reply to client
     if (n < 0) error("ERROR writing to socket");
 
